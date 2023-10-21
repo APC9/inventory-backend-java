8423d313a8a4cf0ae25af180b183520c5db23dd3
APC9 <penacastilloalberto32@gmail.com>
2023-10-21 18:19:14 +0200

    Servicio de obtener todas las categorias

---
+++ src/main/java/com/company/inventory/inventory/respose/CategoryResponseRest.java
@@ -0,0 +1,11 @@
+package com.company.inventory.inventory.respose;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryResponseRest extends ResponseRest {
+
+  private CategoryResponse categoryResponse = new CategoryResponse();
+}
