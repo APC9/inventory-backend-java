0f13b64dd6c1399fed5001ba76b824c7efcb2383
APC9 <penacastilloalberto32@gmail.com>
2023-10-23 15:15:05 +0200

    servicio guardar categoria

--- src/main/java/com/company/inventory/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/inventory/controller/CategoryRestController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.company.inventory.inventory.model.Category;
 import com.company.inventory.inventory.respose.CategoryResponseRest;
 import com.company.inventory.inventory.services.ICategoryService;
 
@@ -35,6 +38,18 @@ public class CategoryRestController {
   @GetMapping("/categories/{id}")
   public ResponseEntity<CategoryResponseRest> sarchCategoryById( @PathVariable Long id ){
     ResponseEntity<CategoryResponseRest> response = service.searchById(id);
+    return response;
+  }
+
+
+  /**
+   * Create a new category
+   * @param category
+   * @return
+   */
+  @PostMapping("/categories")
+  public ResponseEntity<CategoryResponseRest> createCategory( @RequestBody Category category){
+    ResponseEntity<CategoryResponseRest> response = service.createCategory(category);
     return response;
   }
 
--- src/main/java/com/company/inventory/inventory/services/ICategoryService.java
+++ src/main/java/com/company/inventory/inventory/services/ICategoryService.java
@@ -2,10 +2,13 @@ package com.company.inventory.inventory.services;
 
 import org.springframework.http.ResponseEntity;
 
+import com.company.inventory.inventory.model.Category;
 import com.company.inventory.inventory.respose.CategoryResponseRest;
 
 public interface ICategoryService {
   
   public ResponseEntity<CategoryResponseRest> search();
   public ResponseEntity<CategoryResponseRest> searchById( Long id );
+  public ResponseEntity<CategoryResponseRest> createCategory( Category category );
+  
 }
