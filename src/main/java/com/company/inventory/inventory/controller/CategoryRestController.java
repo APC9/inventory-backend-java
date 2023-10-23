4cbd66061bca0410d5f5fb1508c54130ad8b20ce
APC9 <penacastilloalberto32@gmail.com>
2023-10-23 14:34:02 +0200

    Buscar categoria por id

--- src/main/java/com/company/inventory/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/inventory/controller/CategoryRestController.java
@@ -3,6 +3,7 @@ package com.company.inventory.inventory.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +17,24 @@ public class CategoryRestController {
   @Autowired
   private ICategoryService service;
 
+  /**
+   * get all categories
+   * @return
+   */
   @GetMapping("/categories")
   public ResponseEntity<CategoryResponseRest> sarchCategory(){
     ResponseEntity<CategoryResponseRest> response = service.search();
+    return response;
+  }
+
+  /**
+   * get categories by id
+   * @param id 
+   * @return
+   */
+  @GetMapping("/categories/{id}")
+  public ResponseEntity<CategoryResponseRest> sarchCategoryById( @PathVariable Long id ){
+    ResponseEntity<CategoryResponseRest> response = service.searchById(id);
     return response;
   }
 
