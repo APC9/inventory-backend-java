ae88dbec440d6cec5f4195e27c1023e71890850a
APC9 <penacastilloalberto32@gmail.com>
2023-10-23 15:15:05 +0200

    servicio guardar categoria

--- src/main/java/com/company/inventory/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/inventory/services/CategoryServiceImpl.java
@@ -66,6 +66,34 @@ public class CategoryServiceImpl implements ICategoryService {
 
     return new ResponseEntity<CategoryResponseRest>( response, HttpStatus.OK);
   }
+
+  @Override
+  @Transactional
+  public ResponseEntity<CategoryResponseRest> createCategory(Category category) {
+    CategoryResponseRest response = new CategoryResponseRest();
+    List<Category> list = new ArrayList<>();
+
+    try {
+
+      Category newCategory = categoryDao.save(category);
+
+      if( newCategory == null ) {
+        response.setMetadata("Respuesta nok", "-1", "Category not found");
+        return new ResponseEntity<CategoryResponseRest>( response, HttpStatus.BAD_REQUEST);
+      }
+
+      list.add(newCategory);
+      response.getCategoryResponse().setCategory(list);
+      response.setMetadata("Respuesta ok", "00", "Category saved successfully");
+
+    } catch (Exception e) {
+      response.setMetadata("Respuesta nok", "-1", "Error al crear categoria");
+      e.getStackTrace();
+      return new ResponseEntity<CategoryResponseRest>( response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    return new ResponseEntity<CategoryResponseRest>( response, HttpStatus.OK);
+  }
   
 
 }
